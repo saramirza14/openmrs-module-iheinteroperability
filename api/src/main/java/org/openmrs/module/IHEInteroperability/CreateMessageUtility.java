f1d4445a9e6681b7ea08a97fcacd1c01e6c65249
Sara Fatima <sara.mirza14@gmail.com>
2014-06-05 14:00:29 +0530

    Successful HL7 message created

--- api/src/main/java/org/openmrs/module/IHEInteroperability/CreateMessageUtility.java
+++ api/src/main/java/org/openmrs/module/IHEInteroperability/CreateMessageUtility.java
@@ -4,10 +4,9 @@ import java.io.IOException;
 import org.openmrs.Patient;
 
 import ca.uhn.hl7v2.HL7Exception;
-import ca.uhn.hl7v2.model.v24.message.ADT_A01;
-import ca.uhn.hl7v2.model.v24.segment.MSH;
-import ca.uhn.hl7v2.model.v24.segment.PID;
-import ca.uhn.hl7v2.parser.DefaultXMLParser;
+import ca.uhn.hl7v2.model.v25.message.ADT_A05;
+import ca.uhn.hl7v2.model.v25.segment.MSH;
+import ca.uhn.hl7v2.model.v25.segment.PID;
 import ca.uhn.hl7v2.parser.Parser;
 import ca.uhn.hl7v2.parser.PipeParser;
 
@@ -15,29 +14,29 @@ public class CreateMessageUtility {
 	
 public String createHL7Message(Patient patient) throws IOException, HL7Exception {
 		
-	  ADT_A01 adt = new ADT_A01();
+	  ADT_A05 adt = new ADT_A05();
       
       // Populate the MSH Segment
       MSH mshSegment = adt.getMSH();
       mshSegment.getFieldSeparator().setValue("|");
       mshSegment.getEncodingCharacters().setValue("^~\\&");
-      mshSegment.getDateTimeOfMessage().getTimeOfAnEvent().setValue("200701011539");
       mshSegment.getSendingApplication().getNamespaceID().setValue("TestSendingSystem");
       mshSegment.getSequenceNumber().setValue("123");
-      mshSegment.getMessageType().getMessageType().setValue("ADT");
-      mshSegment.getMessageType().getTriggerEvent().setValue("A01");
-      mshSegment.getMessageType().getMessageStructure().setValue("ADT A01");
+      mshSegment.getMessageType().getTriggerEvent().setValue("A05");
+      mshSegment.getMessageType().getMessageStructure().setValue("ADT A05");
       
       // Populate the PID Segment
       PID pid = adt.getPID(); 
-      pid.getPatientName(0).getFamilyName().getSurname().setValue("Doe");
-      pid.getPatientName(0).getGivenName().setValue("John");
-      pid.getPatientIdentifierList(0).getID().setValue("123456");
-
-      /*
-       * In a real situation, of course, many more segments and fields would be populated
-       */
-              
+      pid.getPatientName(0).getFamilyName().getSurname().setValue(patient.getFamilyName());
+      pid.getPatientName(0).getGivenName().setValue(patient.getGivenName());
+      
+      if(patient.getAttribute(2) != null)      
+    	  pid.getBirthPlace().setValue(patient.getAttribute(2).toString());
+      
+      if(patient.getAttribute(3) != null)
+    	  pid.getCitizenship(0).getText().setValue(patient.getAttribute(3).toString());
+      
+          
       // Now, let's encode the message and look at the output
       Parser parser = new PipeParser();
       String encodedMessage = parser.encode(adt);
